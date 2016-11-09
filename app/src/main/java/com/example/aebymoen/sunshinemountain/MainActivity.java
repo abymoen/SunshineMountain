bf1d63723dfa4073f3c1f845c96dca799648048a
aebymoen <a.bymoen@hotmail.com>
2016-11-09 15:42:03 -0700

    First few minutes of working

--- app/src/main/java/com/example/aebymoen/sunshinemountain/MainActivity.java
+++ app/src/main/java/com/example/aebymoen/sunshinemountain/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.aebymoen.sunshinemountain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button skier;
+    Button nature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        skier = (Button) findViewById(R.id.mainSkierButton);
+        nature = (Button) findViewById(R.id.mainNatureButton);
+
+        skier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SkierLandingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        nature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
